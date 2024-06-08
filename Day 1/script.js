d00f47aaa314f2aa5965c0582795c19d383eb4fc
Angelo Destin <angelodestin7@gmail.com>
2024-06-08 17:02:13 +0100

    My Custom

--- Day 1/script.js
+++ Day 1/script.js
@@ -1,12 +1,32 @@
-const panels =document.querySelectorAll('.panel');
-panels.forEach((panel)=>{
-    panel.addEventListener('click',()=>{
-        removeActiveClasses()
+const panels = document.querySelectorAll('.panel');
+const head = document.querySelector('.header');
+const body = document.querySelector('.body');
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        removeActiveHead();
         panel.classList.add('active');
-    })
-})
-function removeActiveClasses(){
-    panels.forEach((panel)=>{
+        panel.style.height = "70vh";
+    });
+});
+
+function removeActiveClasses() {
+    panels.forEach((panel) => {
         panel.classList.remove('active');
-    })
+    });
 }
+
+function removeActiveHead() {
+    head.classList.remove('active');
+}
+
+head.addEventListener('click', () => {
+    removeActiveClasses();
+    panels.forEach(panel => {
+        panel.style.height = "10vh";
+        panel.style.transition = " 0.7s ease-in";
+        panel.style.marginTop = "-10px";
+    });
+    head.classList.add('active');
+});
